3251aa73e65aecee345255887d16f6cf06ee8f24
James Churchill <james@smashdev.com>
2020-10-13 13:35:49 -0700

    Update App class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,8 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hi Alexis");
+
+        System.out.println("Whats the object-oriented way to become wealthy?");
+        System.out.println("Inheritance");
     }
 }
